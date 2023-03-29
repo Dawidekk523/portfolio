91acf7c213932504fd314c650ea4191ba7ad8e92
dawidekk523 <daveedsgn@gmail.com>
2023-03-29 15:15:01 +0200

    1.5 Finally routes done!

---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AiFillGithub } from 'react-icons/ai';
+
+
+const footer = () => {
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+export default footer;
